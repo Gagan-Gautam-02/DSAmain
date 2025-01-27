afa0ac12e3afb58bb305dfd5532138a6f197bd05
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-01-27 23:22:35 +0530

    code

--- Intro.cpp
+++ Intro.cpp
@@ -1,10 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-   int x;
-   cin >> x;
-   cout << "hey" << x;
-   
+int main() {
+    int number; // Variable to store the integer input
 
+    // Prompt the user for input
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    // Print the entered integer
+    cout << "You entered: " << number << endl;
+
+    return 0;
 }
