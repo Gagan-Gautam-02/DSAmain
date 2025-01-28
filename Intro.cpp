9081885ef224e0ed20798b71c6a40a447dada869
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-01-28 23:40:28 +0530

    array

--- Intro.cpp
+++ Intro.cpp
@@ -57,19 +57,23 @@ int main(){
    //    cout << "eligible for retirement";
    // }
 
-   int day;
-   cin >> day;
-    switch(day){
-       case 1: cout << "Monday"; break;
-       case 2: cout << "Tuesday"; break;
-       case 3: cout << "Wednesday"; break;
-       case 4: cout << "Thursday"; break;
-       case 5: cout << "Friday"; break;
-       case 6: cout << "Saturday"; break;
-       case 7: cout << "Sunday"; break;
-       default: cout << "Invalid day";
-    }
-    
+   // int day;
+   // cin >> day;
+   //  switch(day){
+   //     case 1: cout << "Monday"; break;
+   //     case 2: cout << "Tuesday"; break;
+   //     case 3: cout << "Wednesday"; break;
+   //     case 4: cout << "Thursday"; break;
+   //     case 5: cout << "Friday"; break;
+   //     case 6: cout << "Saturday"; break;
+   //     case 7: cout << "Sunday"; break;
+   //     default: cout << "Invalid day";
+   //  }
+
+   int arr[5];
+   cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+   cout << arr[0] << arr[1] << arr[2] << arr[3] << arr[4];
+
 
    return 0;
 }
