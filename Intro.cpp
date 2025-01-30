f88b122e8673c930e8a4fdc1ae6264f8e794486c
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-01-30 23:45:33 +0530

    functions

--- Intro.cpp
+++ Intro.cpp
@@ -110,13 +110,44 @@ using namespace std;
    // }
 
 
-void PrintName(string name){
-      cout << "some text" << name << endl;
-   }
+// void PrintName(string name){
+//       cout << "some text" << name << endl;
+//    }
 
-   int main(){
-      string name;
-      cin >> name;
-      PrintName(name);
-      return 0;
-   }
+//    int main(){
+//       string name;
+//       cin >> name;
+//       PrintName(name);
+//       return 0;
+//    }
+
+
+// int sum(int num1, int num2){
+//    int num3 = num1 + num2;
+//    return num3;
+// }
+
+// int main(){
+//    int num1, num2;
+//    cin >> num1 >> num2;
+//    int res = sum (num1, num2);
+//    cout << res;
+
+//    return 0;
+// }
+
+
+void something(int num){
+   cout << num << endl;
+   num += 10;
+   cout << num << endl;
+   num +=5;
+   cout << num << endl;
+}
+
+int main(){
+   int num = 10;
+   something(num);
+   cout << num;
+   return 0;
+}
