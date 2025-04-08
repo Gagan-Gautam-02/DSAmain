7a4ce9703d261f3ebca63e3501d8736b7ebb33aa
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-04-09 01:17:08 +0530

    Learn Pattern

--- Cpp/ConStm.cpp
+++ Cpp/ConStm.cpp
@@ -29,8 +29,15 @@ using namespace std;
 
 
 int main(){
-    int a,b;
+    int a;
+    cin>>a;
     for(int i = 2; i<= a-1; i++){ 
-        if()
+        if( a%i == 0){
+            cout<<"not prime"<<endl;
+            break;
+        }else{
+            cout<<"prime"<<endl;
+            break;
+        }
     }
 }
---
+++ Cpp/Pattern.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+using namespace std;
+
+// int  main(){
+//     int n;
+//     cin>>n;
+//     for(int i = 1; i<=n; i++){
+//         for(int j = 1; j<=n; j++){
+//             cout<<"^";
+//         }
+//         cout<<endl;
+//      }
+// }
+
+// int main(){
+//     char n;
+//     cin>>n;
+//     for(int i = 'a'; i<=n; i++){
+//         for(int j = 'a'; j<=n; j++){
+//             cout<<j;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// int main(){
+//     int n = 10;
+//     for(int i = 0; i<=n; i++){
+//         char ch = 'A' + i;
+//         for(int j = 0; j<=i; j++){
+//             cout<<ch;
+//             ch = ch + 1;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// int main(){
+//     int n = 5;
+//     for(int i = 0; i<=n; i++){
+//         for(int j = 0; j<i+1; j++){
+//             cout<<"^*";
+//         }
+//         cout<<endl;
+//     }return 0;
+// }
+
+
+// int main() {
+//     cout << "       _==/          i     i          \\==_        " << endl;
+//     cout << "     /XX/            |\\___/|            \\XX\\      " << endl;
+//     cout << "   /XXXX\\            |XXXXX|            /XXXX\\    " << endl;
+//     cout << "  |XXXXXX\\_         _XXXXXXX_         _/XXXXXX|   " << endl;
+//     cout << " XXXXXXXXXXXxxxxxxxXXXXXXXXXXXxxxxxxxXXXXXXXXXXX  " << endl;
+//     cout << "|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX| " << endl;
+//     cout << "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX " << endl;
+//     cout << "|XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX| " << endl;
+//     cout << " XXXXXX/^^^^\"\\XXXXXXXXXXXXXXXXXXXXX/^^^^^\\XXXXXX  " << endl;
+//     cout << "  |XXX|       \\XXX/^^\\XXXXX/^^\\XXX/       |XXX|   " << endl;
+//     cout << "    \\XX\\       \\X/    \\XXX/    \\X/       /XX/     " << endl;
+//     cout << "       \"\\       \"      \\X/      \"       /\"        " << endl;
+//     cout << "                    THE BATMAN                   " << endl;
+
+//     return 0;
+// }
+
+  
+// int main(){
+//     int n = 5;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<=i; j++){
+//             cout<<j+1;
+//         }
+//         cout<<endl;
+//     }
+// }
+
+// //Floyd's Triangle
+// int main(){
+//     int n = 5;
+//     int count = 1;
+//     for(int i = 0; i<n; i++){
+//         for(int j = 0; j<=i; j++){
+//             cout<<count<<" ";
+//             count++;
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
