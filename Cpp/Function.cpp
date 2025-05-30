014662af8d97070718ca9c863f1164b2f248c323
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-05-30 23:00:21 +0530

    ADD new files

--- Cpp/Function.cpp
+++ Cpp/Function.cpp
@@ -2,14 +2,54 @@
 #include<iostream>
 using namespace std;
 
-int Add(int a, int b){
-    return a + b;
-}
+// int Add(int a, int b){
+//     return a + b;
+// }
 
-int main(){
-    int a, b;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
-    cout << "Sum: " << Add(a, b) << endl;
-    return 0;
+// int main(){
+//     int a, b;
+//     cout << "Enter two numbers: ";
+//     cin >> a >> b;
+//     cout << "Sum: " << Add(a, b) << endl;
+//     return 0;
+// }
+
+// int MaxNum(int a, int b){
+//     if(a>b){
+//         cout<<"Max number is: " << a << endl;
+//     }else{
+//         cout<<"Max number is: " << b << endl;
+//     }
+//     return 0;
+// }
+
+// int main(){
+//     int a, b;
+//     cin >> a >> b;
+//     cout << "Max number is: " << MaxNum(a, b) << endl;
+//     return 0;
+// }
+
+
+// int SumDigit(int n) {
+//     int sum = 0;
+//     while (n != 0) {
+//         sum += n % 10;  // Add the last digit of n to sum
+//         n = n / 10;     // Remove the last digit from n
+//     }
+//     return sum;
+// }
+
+// int main() {
+//     int n;
+//     cin >> n;
+//     cout << "Sum of digits: " << SumDigit(n) << endl;
+//     return 0;
+// }
+
+// Decimal to Binary
+
+int CovDecBin(int n){
+    int ans = 0;
+    
 }
