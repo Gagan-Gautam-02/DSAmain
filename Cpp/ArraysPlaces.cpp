a6cdb8c855119bda90e2aebac1fb0080142d474f
Gagan-Gautam-02 <gg5202@srmist.edu.in>
2025-07-19 23:43:42 +0530

    fix error

--- Cpp/ArraysPlaces.cpp
+++ Cpp/ArraysPlaces.cpp
@@ -1,21 +1,21 @@
 #include<iostream>
 using namespace std;
  
-int main() {
-    int arr[5] = {1, 2, 3, 4, 5};
-    int *ptr = arr; // Pointer to the first element of the array
+// int main() {
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     int *ptr = arr; // Pointer to the first element of the array
 
-    cout << "Array elements using pointer: ";
-    for (int i = 0; i < 5; i++) {
-        cout << *(ptr + i) << " "; // Accessing elements using pointer arithmetic
-    }
-    cout << endl;
+//     cout << "Array elements using pointer: ";
+//     for (int i = 0; i < 5; i++) {
+//         cout << *(ptr + i) << " "; // Accessing elements using pointer arithmetic
+//     }
+//     cout << endl;
 
-    cout << "Array elements using array notation: ";
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " "; // Accessing elements using array notation
-    }
-    cout << endl;
+//     cout << "Array elements using array notation: ";
+//     for (int i = 0; i < 5; i++) {
+//         cout << arr[i] << " "; // Accessing elements using array notation
+//     }
+//     cout << endl;
 
-    return 0;
-}
+//     return 0;
+// }
